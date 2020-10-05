0f84fd62db26f67de3110236ac153864464dddee
chyppoli <claude.hyppolite@gmail.com>
2020-10-05 07:45:40 -0400

    add conditional styling to textarea based on the current hour

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
 var currDt = moment().format('LLLL');
-var currentHr = parseInt(13) //moment().format('h');
+var currentHr = 11//parseInt(moment().format('h'));
 var timeBlockEl = $(".time-block");
 var workStartHr = 9;
-var workEndHr = 13;
+var workEndHr = 17;
 var col1El = $("#col-1");
 var col2El = $("#col-2");
 var col3El = $("#col-3");
@@ -23,50 +23,52 @@ var col3El = $("#col-3");
             var newCol1 = $("<textarea>");
             var newCol2 = $("<div>");
             var inputEl = $("<div>")
-            var saveButton = $("<div>");
+            var saveButton = $("<button>");
             var pEl = $("<p>");
+
             
-            // saveButton.addClass("btn btn-primary saveBtn");
-            // saveButton.attr("type","button");
+            saveButton.addClass("saveBtn i")
+            saveButton.attr("type","button");
             saveButton.text("Save");
          
             newRow.addClass("row");
+            newCol1.attr("data-index",i);
             newCol.addClass("col-2 hour");
             newCol.text(strHr);
             newRow.append(newCol);
 
             newCol1.addClass("col-7");
             
-            pEl.addClass("pt-4");
+            // pEl.addClass("pt-4");
             pEl.text("Save");
-            // newCol1.text("enter tasks here");
 
-            newCol2.addClass("col-2 saveBtn");
-            // newCol2.append(saveButton);
-            newCol2.append(pEl);
+
+            newCol2.addClass("col-2 p-0");
+            newCol2.append(saveButton);
 
             
             timeBlockEl.append(newRow);
             newRow.append(newCol1);
             newRow.append(newCol2);
 
-        //     // styles description based on whether or not the hour is current
-        //     if (currentHr === i){
-        //         $(".textarea").addClass("present");
-        //         console.log("current hour is " + i);
-        //     }
-        //     else if (currentHr < i && currentHr != i){
-        //        $(".textarea").addClass("past");
-        //         console.log("past hour is " + i);
-        //     }
-        //     else {$("textarea").addClass("future")
-        // console.log("future hour is " + i)}
+            // console.log("current hour is " + currentHr);
+            // console.log("current i is :" + i);
 
-            // descP.text("Testing 1-2-3");
-            // timeP.text(strHr);
-            // $(".float-left").append(timeP);
-            // $(".float-none").append(descP);
 
+      
+
+        // apply style to respective text area based on where or not time has past
+            if (i > parseInt(currentHr)){
+                $("textarea[data-index='" + i + "']").addClass("future");
+                 
+            }
+            else if (i == parseInt(currentHr)) {
+                $("textarea[data-index='" + i + "']").addClass("present");
+            }
+
+            else {  $("textarea[data-index='" + i + "']").addClass("past");
+        }
+       
             
 
 
